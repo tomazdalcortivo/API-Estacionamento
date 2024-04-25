90e6fd5ba5b5e625700268837750492e1ff993b2
tomazdalcortivo <p12547254@gmail.com>
2024-04-25 15:24:12 -0300

    Recurso para listar todos os usuários

--- src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
@@ -2,9 +2,12 @@ package com.mballem.demoparkapi.service;
 
 import com.mballem.demoparkapi.entity.Usuario;
 import com.mballem.demoparkapi.repositoy.UsuarioRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 // criar um contrutor com a variavel
 @RequiredArgsConstructor
@@ -28,5 +31,10 @@ public class UsuarioService {
         Usuario user = buscarPorid(id);
         user.setPassword(password);
         return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
     }
 }
