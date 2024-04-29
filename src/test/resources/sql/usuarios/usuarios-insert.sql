87f50fa92e98526b41203b66a73c95302f36421b
tomazdalcortivo <p12547254@gmail.com>
2024-04-29 17:36:18 -0300

    Configurando o ambiente de testes

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS (id, username, password, role) values (100, "ana@gmail.com", "123456", "ROLE_ADMIN");
+insert into USUARIOS (id, username, password, role) values (101, "pablo@gmail.com", "123456", "ROLE_CLIENTE");
+insert into USUARIOS (id, username, password, role) values (102, "robert@gmail.com", "123456", "ROLE_CLIENTE");
